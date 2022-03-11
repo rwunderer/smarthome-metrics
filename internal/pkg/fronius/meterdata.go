ce568124b1de6a66d78728e2909868ba541d26b2
Robert Wunderer <robert.wunderer@caprisys.at>
2022-03-11 17:27:50 +0000

    Add meter values calculation

--- internal/pkg/fronius/meterdata.go
+++ internal/pkg/fronius/meterdata.go
@@ -41,9 +41,14 @@ func (controller *FroniusController) getMeterData(ctx context.Context) error {
 		return nil
 	}
 
+	meterIn := d.Body.Data.Consumed + float64(controller.Config.MeterOffsetIn)
+	meterOut := d.Body.Data.Produced + float64(controller.Config.MeterOffsetOut)
+
 	log.WithFields(log.Fields{
 		"consumed":  int(d.Body.Data.Consumed),
 		"produced":  int(d.Body.Data.Produced),
+		"meterIn": int(meterIn),
+		"meterOut": int(meterOut),
 		"timestamp": d.Head.Timestamp,
 	}).Debug("Successfully parsed Meter data")
 
