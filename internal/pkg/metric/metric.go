9e8b8de2d3650743f232167e240c6c8466819f2b
Robert Wunderer <robert.wunderer@caprisys.at>
2022-03-25 11:14:36 +0000

    Use struct for metrics

--- internal/pkg/metric/metric.go
+++ internal/pkg/metric/metric.go
@@ -1,7 +1,38 @@
 package metric
 
-type Metric map[string] float64
+import (
+	"sync"
+	"time"
+)
 
-func NewMetric() Metric {
-	return make(Metric)
+type Metric struct {
+	value float64
+	time  time.Time
+}
+
+type Metrics struct {
+	metrics map[string]Metric
+	lock    sync.RWMutex
+}
+
+func NewMetrics() Metrics {
+	return make(Metrics)
+}
+
+func (Metrics *m) Set(key string, value float64) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.metrics[key] = Metric{
+		value: value,
+		time:  time.Now(),
+	}
+}
+
+func (Metrics *m) Get(key string) Metric {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	rv := m.metrics[key]
+	return rv
 }
